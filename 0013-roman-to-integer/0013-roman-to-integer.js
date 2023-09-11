e99d08fca50e55fd7e47196b572bdbe60d4f60bc
Seo Jeong Woo <64897060+teapotsoup@users.noreply.github.com>
2023-09-11 11:06:55 +0900

    Time: 119 ms (40.51%), Space: 47.9 MB (32.41%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -1,19 +1,53 @@
-function romanToInt( s) {
-    let ans = 0;
-    let num = 0;
-   for (let i = s.length-1; i >= 0; i--) {
-       switch(s.charAt(i)) {
-           case 'I': num = 1; break;
-           case 'V': num = 5; break;
-           case 'X': num = 10; break;
-           case 'L': num = 50; break;
-           case 'C': num = 100; break;
-           case 'D': num = 500; break;
-           case 'M': num = 1000; break;
-       }
-       if (4 * num < ans) ans -= num;
-       else ans += num;
-   }
-   console.log(ans)
-   return ans;
-}
+var romanToInt = function(s) {
+    const arr = s.toString().split('');
+    const numArr = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 'I'){
+            if (arr[i]+arr[i+1] === 'IV'){
+                numArr.push(4)
+                i=i+1
+            }else if(arr[i]+arr[i+1] === 'IX'){
+                numArr.push(9)
+                i=i+1
+            }
+            else{
+                numArr.push(1)
+            }
+        }else if(arr[i] === 'X'){
+            if (arr[i]+arr[i+1] === 'XL'){
+                numArr.push(40)
+                i=i+1
+            }else if(arr[i]+arr[i+1] === 'XC'){
+                numArr.push(90)
+                i=i+1
+            }
+            else{
+                numArr.push(10)
+            }
+        }else if(arr[i] === 'C'){
+            if (arr[i]+arr[i+1] === 'CD'){
+                numArr.push(400)
+                i=i+1
+            }else if(arr[i]+arr[i+1] === 'CM'){
+                numArr.push(900)
+                i=i+1
+            }else{
+                numArr.push(100)
+            }
+        }
+        else if(arr[i] === 'V'){
+            numArr.push(5)
+        }
+        else if(arr[i] === 'L'){
+            numArr.push(50)
+        }
+        else if(arr[i] === 'D'){
+            numArr.push(500)
+        }
+        else if(arr[i] === 'M'){
+            numArr.push(1000)
+        }
+    }
+    let result = numArr.reduce((a,b)=>a+b,0)
+    return result
+};
