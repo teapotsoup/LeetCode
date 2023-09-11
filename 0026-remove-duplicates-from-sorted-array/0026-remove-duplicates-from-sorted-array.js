45a8f5da06c628c36693f4ab4f07132d732613c2
Seo Jeong Woo <64897060+teapotsoup@users.noreply.github.com>
2023-09-11 14:29:38 +0900

    Time: 117 ms (19.01%), Space: 44.3 MB (75.20%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    for(let i=0; i < nums.length - 1;){
+        // console.log(nums , i)
+       if(nums[i] == nums[i+1] ){
+           nums.splice(i,1)
+       } else{
+           i++
+       }
+    }  
+};
